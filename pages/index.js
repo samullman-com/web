1eeedae682fce090dac880d99c001d25561ba501
Sam <samullman@gmail.com>
2021-01-27 18:31:35 -0800

    handle variables betta

--- pages/index.js
+++ pages/index.js
@@ -30,8 +30,8 @@ import jump from "jump.js";
 
 
 const client = require('contentful').createClient({
-  space: process.env.CONTENTFUL_SPACE_ID ,
-  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN ,
+  space: `${process.env.CONTENTFUL_SPACE_ID}` ,
+  accessToken: `${process.env.CONTENTFUL_ACCESS_TOKEN}`,
 });
 
 
