5b15a2a1754eda3338900a3dbcb02c453ed16e64
Sam <samullman@gmail.com>
2021-01-28 15:03:22 -0800

    analytics

--- components/layout.js
+++ components/layout.js
@@ -1,6 +1,11 @@
+import { initGA, logPageView } from '../utils/analytics'
+
 import Head from 'next/head';
 import Header from "../components/header";
 import Footer from "../components/footer";
+
+
+
 
 import {
   Box, 
@@ -11,16 +16,27 @@ import { BiUpArrowAlt } from "react-icons/bi";
 import ScrollToTop from "react-scroll-to-top";
 
 
-function Layout ( props ) {
+class Layout extends React.Component  {
+  componentDidMount ()  {
+    if (!window.GA_INITIALIZED) {
+      initGA()
+      window.GA_INITIALIZED = true
+    }
+    logPageView()
+  }
+
+  
+  render () {
+
     return < >
     <Head>
-      <title>{ props.title }</title>
+      <title>{ this.props.title }</title>
       <link rel="icon" href="/favicon.ico" />
     </Head>
 
     <Header />
 
-    <Box pt={[ "60px", "60px", "70px"]}> 
+    <Box pt={[ "60px", "60px", "70px"]} bg="gray.100"> 
 
     <ScrollToTop smooth color="#6f00ff" component={ 
       <Button textAlign="center" width="40px" p={0} fontSize={24} variant="ghost">
@@ -30,12 +46,15 @@ function Layout ( props ) {
 
 
     <main>
-        { props.children }
+        { this.props.children }
     </main>
     </Box>
 
     <Footer />
   </>
+
+  }
+    
 }
 
 export default Layout;
