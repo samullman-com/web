1a8ecc56917d346bcdff0a4c9c64e64f95ac1875
Sam <samullman@gmail.com>
2021-01-27 17:57:41 -0800

    environment

--- pages/index.js
+++ pages/index.js
@@ -27,6 +27,8 @@ import {
 import VerticalAlign from "../components/verticalAlign";
 import jump from "jump.js";
 
+
+console.log( process.env )
 const client = require('contentful').createClient({
   space: process.env.CONTENTFUL_SPACE_ID,
   accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
